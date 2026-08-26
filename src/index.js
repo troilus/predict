98371bf613912874edc616537662ace4f7ca1a19
troilus <iswangli@outlook.com>
2026-08-26 12:00:39 +0800

    卫星ID显示bugfix

--- src/index.js
+++ src/index.js
@@ -709,8 +709,8 @@ function populateDropdown(satellites) {
         dropdown.innerHTML = ''; // 清空列表
         filteredSatellites.forEach(satellite => {
             const div = document.createElement('div');
-            const noradId = satellite.tle[1].split(' ')[1];
-            div.textContent = `${satellite.name}[${noradId}]`;
+            const noradId = satellite.tle[1]?.split(' ')[1];
+            div.textContent = noradId ? `${satellite.name}[${noradId}]` : satellite.name;
             div.classList.add('dropdown-item');
             div.addEventListener('click', function () {
                 searchInput.value = satellite.name; // 将选择的值填入输入框
